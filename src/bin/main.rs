7b6fcad0cedb93474fbe0481ac91e02a3c473a3f
toshokan <toshokan@shojigate.net>
2019-01-12 21:19:03 -0500

    Added option to read from file

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,19 +1,13 @@
 extern crate eachdo;
-
 extern crate clap;
 
 
 fn main() {
-    clap::App::new("eachdo")
-        .version("0.1.0")
-        .author("toshokan <toshokan@shojigate.net>")
-        .arg(clap::Arg::with_name("file")
-             .short("f")
-             .long("file")
-             .value_name("FILE")
-             .help("Read input from FILE rather than stdin")
-             .takes_value(true))
-        .get_matches();
+    let config = eachdo::Config::new();
+    
+    if let Some(file) = config.filename {
+        println!("Got a filename! {:?}", file);
+    }
     
     println!("Done!");
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,37 @@
+use std::path;
+
+pub struct Config {
+    pub filename: Option<path::PathBuf>,
+}
+
+impl Config {
+    pub fn new() -> Config {
+        let matches = clap::App::new("eachdo")
+            .version("0.1.0")
+            .author("toshokan <toshokan@shojigate.net>")
+            .arg(clap::Arg::with_name("file")
+                 .short("f")
+                 .long("file")
+                 .value_name("FILE")
+                 .help("Read input from FILE rather than stdin")
+                 .takes_value(true))
+            .get_matches();
+
+        let mut filename = None;
+
+        if matches.is_present("file") {
+            if let Some(provided_filename) = matches.value_of("file") {
+                filename = Some(path::PathBuf::from(provided_filename));
+            }
+        }
+
+        Config {
+            filename
+        }
+    }
+}
+
+
 #[cfg(test)]
 mod tests {
     #[test]
